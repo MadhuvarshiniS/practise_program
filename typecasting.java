142cde06014e4f5b5ffd8fc598b6010a6e309317
MadhuvarshiniS <109650018+MadhuvarshiniS@users.noreply.github.com>
2022-07-26 14:05:20 +0530

    Add files via upload

---
+++ typecasting.java
@@ -0,0 +1,17 @@
+package typeCasting;
+import java.util.Scanner;
+
+public class typecasting {
+	public static void main(String[] args) {
+		int x;
+		Scanner d = new Scanner(System.in);
+		System.out.print("Enter a numbers : ");
+		x=d.nextInt();
+		long y=x;
+		float c=y;
+		System.out.println("Before type casting ,int value:" +x);
+		System.out.println("After type casting ,long value:" +y);
+		System.out.println("After type casting ,float value:" +c);
+	}
+
+}
